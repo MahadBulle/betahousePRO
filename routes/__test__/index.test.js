3990c3d5e0719fa2c76c657280f7392396667f7e
MahadBulle <mahadhussein012@gmail.com>
2023-07-25 13:23:48 -0700

    bbbbbb

--- routes/__test__/index.test.js
+++ routes/__test__/index.test.js
@@ -1,4 +1,4 @@
-const app = require('../../app')
+const app = require('../..')
 const request = require('supertest')
 // test houses api
 //  describe('testing houses api crud operations', function () {
